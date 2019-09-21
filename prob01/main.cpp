0155b158d9b6984659f7d2bbd8fe003a5910b61c
stsan <stsan@cs104u-1e.acad.fullerton.edu>
2019-09-21 13:31:49 -0700

    comments added

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -32,6 +32,10 @@ int main()
   std::cout << sizeof(d2) << " bytes, value: " << d2 << std::endl;
   std::cout << sizeof(f2) << " bytes, value: " << f2 << std::endl;
 
+  // As the values get closer to 0, their difference increases.
+  // Float comes closer then Double value to 0,
+  // indicating that Float has a higher rate of change than Double.
+
   d2 *= 0.1;
   f2 *= 0.1;
 
@@ -39,6 +43,9 @@ int main()
   std::cout << "\n**comment**" << std::endl;
   std::cout << sizeof(d2) << " bytes, value: " << d2 << std::endl;
   std::cout << sizeof(f2) << " bytes, value: " << f2 << std::endl;
+
+  // Here, the double value is minimally changed while the float value has
+  // become 0.
 
   double d3 = 3.5E38;
   float f3 = 3.5E38;
@@ -48,6 +55,10 @@ int main()
   std::cout << std::setprecision(1);
   std::cout << sizeof(d3) << " bytes, value: " << d3 << std::endl;
   std::cout << sizeof(f2) << " bytes, value: " << f3 << std::endl;
+
+  // Both double and float meet their minimum limit and automatically change to
+  // be at their maximum range. While double is restricted,
+  // float's maximum limit is infinite.
 
   return 0;
 }
