48022fdda5a12a10f2902ff3e9e10e82787cf841
stsan <stsan@cs104u-1e.acad.fullerton.edu>
2019-09-21 14:24:41 -0700

    rectangle program made

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,28 @@
 // Random Rectangle
+#include <ctime>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+int main()
+{
+  // Attain random seed value from time(0)
+  unsigned seed = time(0);
+  srand(seed);
+  // establish base; make width modulus'd by length
+
+  const int length = 40+1;
+  int width = rand() % length;
+
+  // Assign the result of width to the number of #'s presented
+  std::string hashtag;
+  hashtag.assign(width,'#');
+
+  // Print #'s
+  std::cout << hashtag << '\n';
+  std::cout << hashtag << '\n';
+  std::cout << hashtag << '\n';
+
+
+  return 0;
+}
